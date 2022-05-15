670e17fb82cc45b0e3b1585f8e195a3f25e6e932
phuvinhbmt <phuvinhbmt@gmail.com>
2022-05-16 08:34:35 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+Prerequisite
+Node (>=14)
+npm
+Refer to Installation
+
+How to run
+npm install
+npm start 
+
+a successful running process is: Backend is listening on port localhost:3000
+
+Installation
+node -v
+npm -v
+
+Modules used
+express
+axios
---
+++ index.js
@@ -0,0 +1,20 @@
+const scalapayAPI = require('./src/controllers/ScalapayAPIController')
+const express = require('express')
+const PORT = process.env.PORT || 3000;
+
+const app = express()
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+})
+app.use(express.json())
+
+// set router
+app.get('', (req, res) => {res.json({status: 200})})
+app.post('/orders', (req, res) => scalapayAPI.handleOrderCreation(req, res))
+
+app.listen(PORT, () => {
+    console.log(`Backend is listening on port localhost:${PORT}`);
+});
