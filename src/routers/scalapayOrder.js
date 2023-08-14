bd4297cfe061330f1b413b8cc49dcb4f2c8aa9cb
vinh.nguyen <vinh.nguyen@service.nsw.gov.au>
2023-08-14 22:54:33 +1000

    eslint: edit indent

--- src/routers/scalapayOrder.js
+++ src/routers/scalapayOrder.js
@@ -3,7 +3,7 @@ const scalapayAPI = require('../controllers/ScalapayAPIController');
 
 const router = new express.Router();
 router.get('', (req, res) => {
-    res.json({status: 200});
+  res.json({status: 200});
 });
 router.post('/orders', (req, res) => scalapayAPI.handleOrderCreation(req, res));
 
