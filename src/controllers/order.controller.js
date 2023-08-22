f0b155191239eb4605d34ea060bc01ce506d6361
vinh.nguyen <vinh.nguyen@service.nsw.gov.au>
2023-08-22 23:51:51 +1000

    layered-architecture: split scalapayOrderController into controller and services

---
+++ src/controllers/order.controller.js
@@ -0,0 +1,10 @@
+const {createScalapayOrder: postScalapayOrder} = require('../services/order.service');
+
+const createOrder = async (req, res) => {
+  const response = await postScalapayOrder(req);
+  res.status(201).send(response);
+};
+
+module.exports = {
+  createOrder,
+};
